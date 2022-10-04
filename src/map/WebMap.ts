48224d1ea0f90cdc3ea025c18b38a5d88d206bf1
sbuscher <shane@buscherhood.com>
2022-10-03 20:26:09 -0500

    cleanup

--- src/map/WebMap.ts
+++ src/map/WebMap.ts
@@ -26,18 +26,10 @@ export class WebMap {
     }
 
     const webmap = this._createWebMap(options.portalItemId); 
-
-    const { basemap, container, center, zoom } = options;
-    const viewOptions = {basemap: basemap, container: container, center: center, zoom: zoom}
-
-    this._mapView = await this._mapService.buildMapView(webmap, viewOptions);
+    this._mapView = await this._mapService.buildMapView(webmap, options);
   }
 
   _createWebMap = (portalItemId: string) => {
-    return new ArcGISWebMap({
-      portalItem: {
-        id: portalItemId
-      } 
-    });
+    return new ArcGISWebMap({ portalItem: { id: portalItemId } });
   }
 }
